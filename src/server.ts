d8478a06fb3a4fb7ce72cd04b24281d222ae69f3
ecthon <ecthon@gmail.com>
2024-07-09 00:27:22 -0400

    feature: add and config fastify cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import fastify from "fastify";
+import cors from "@fastify/cors"
 import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
 
 const app = fastify()
+
+app.register(cors, {
+  origin: '*',
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
