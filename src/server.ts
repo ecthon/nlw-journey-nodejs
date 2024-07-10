0f74ecd5ae265c82a9a611e1589444da73b9505a
ecthon <ecthon@gmail.com>
2024-07-09 22:14:26 -0400

    feature/routes: create route get-ativities

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod
 import { createTrip } from "./routes/create-trip";
 import { confirmTrip } from "./routes/confirm-trip";
 import { confirmParticipants } from "./routes/confirm-participant";
+import { getActivities } from "./routes/get-activities";
+import { createActivity } from "./routes/create-activity";
 
 const app = fastify()
 
@@ -17,6 +19,8 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(createTrip)
 app.register(confirmTrip)
 app.register(confirmParticipants)
+app.register(createActivity)
+app.register(getActivities)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server running!')
